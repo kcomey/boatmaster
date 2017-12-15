4bb83d8b6d98167f2ea79d7e0744a270db0b05f3
Kendall Comey <kendall.comey@gmail.com>
2017-12-14 19:09:01 -0500

    Added budget page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { BudgetPage } from '../pages/budget/budget';
+
 @Component({
   templateUrl: 'app.html'
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BudgetPage } from '../budget/budget';
+import { ChecklistPage } from '../checklist/checklist';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  showBudgetPage() {
+    this.navCtrl.push(BudgetPage);
+  }
+
+  showChecklistPage() {
+    this.navCtrl.push(ChecklistPage);
   }
 
 }
